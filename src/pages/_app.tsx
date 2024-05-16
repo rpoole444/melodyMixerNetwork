42ca1aab80a7c763bd9240b4f0b07b27d7c55c23
Reid Poole <poole.reid@gmail.com>
2024-05-16 13:23:07 -0600

    Use Provider by wrapping our component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { UserProvider } from '../contexts/UserContext';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { EdgeStoreProvider } from '../lib/edgestore';
 import "../styles/globals.css";
 
 const queryClient = new QueryClient();
@@ -10,7 +11,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <UserProvider>
         <div className="body-bg min-h-screen">
-          <Component {...pageProps} />
+          <EdgeStoreProvider>
+            <Component {...pageProps} />
+          </EdgeStoreProvider>
         </div>
       </UserProvider>
     </QueryClientProvider>
