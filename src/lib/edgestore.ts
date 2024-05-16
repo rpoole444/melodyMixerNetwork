76425a3a5a5606333cb2663e188c213707ba701e
Reid Poole <poole.reid@gmail.com>
2024-05-16 13:22:42 -0600

    Create Edge Store Router and Provider

---
+++ src/lib/edgestore.ts
@@ -0,0 +1,5 @@
+import { createEdgeStoreProvider } from '@edgestore/react';
+import { type EdgeStoreRouter } from '../pages/api/edgestore/[...edgestore]';
+
+const { EdgeStoreProvider, useEdgeStore } = createEdgeStoreProvider<EdgeStoreRouter>();
+export { EdgeStoreProvider, useEdgeStore };
